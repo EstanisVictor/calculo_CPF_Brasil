b9393190e52d8c72f344b4349732dd4a470c5b2e
EstanisVictor <victorestanislau1@gmail.com>
2022-06-04 17:19:01 -0300

    firstCommit

---
+++ src/calculoBrasilCPF.java
@@ -0,0 +1,117 @@
+public class calculoBrasilCPF {
+
+    public static int primeiroDigito = 0;
+    public static int segundoDigito = 0;
+    public static int multiplicadorCrescente = 2;
+    public static String cpf = "00000000000";
+    public static long contadorCPFvalido = 0L;
+    
+    //metodo que gera cpf
+    public static void geradorCPF() {
+
+        for (long i = 0; i <= 99999999999L; i++) {
+            String indexChar = Long.toString(i);
+
+            StringBuilder cpfStringBuilder = new StringBuilder(cpf);
+
+            cpfStringBuilder = cpfStringBuilder.delete((cpf.length() - 1) - indexChar.length(), (cpf.length() - 1));
+
+            cpfStringBuilder = cpfStringBuilder.append(indexChar);
+
+            //priemeiro digito true
+            if(calculaPrimeiroDigito(new String(cpfStringBuilder)) && 
+            calculaSegundoDigito(new String(cpfStringBuilder))){
+                    //os dois digitos verificadores são verdadeiros, logo cpf válido
+                    contadorCPFvalido++;
+                    System.out.println(cpfStringBuilder);
+            }
+            primeiroDigito = 0;
+            segundoDigito = 0;
+        }
+
+    }
+
+    public static boolean calculaPrimeiroDigito(String cpf) {
+        int vetCPF[] = new int[9];
+        String posicaoNumero[] = cpf.split("");
+
+        for (int i = 0; i < vetCPF.length; i++) {
+            vetCPF[i] = Integer.parseInt(posicaoNumero[i]);
+        }
+
+        for (int i = (vetCPF.length - 1); i >= 0; i--) {
+            primeiroDigito += vetCPF[i] * multiplicadorCrescente;
+            multiplicadorCrescente++;
+        }
+
+        // atualizando para 2 novamente
+        multiplicadorCrescente = 2;
+
+        if (primeiroDigito % 11 < 2) {
+            // digito igual a zero
+            if (posicaoNumero[9].equals("0")) {
+                // priemiro digito igual a zero
+                return true;
+            } else {
+                // primeiro digito não é igual a zero
+                return false;
+            }
+        } else {
+            if (primeiroDigito % 11 >= 2) {
+                // digito igual a 11 - resto da divisão
+                int resultadoPrimeiroDigito = 11 - (primeiroDigito % 11);
+
+                if (posicaoNumero[9].equals(Integer.toString(resultadoPrimeiroDigito))) {
+                    // primeiro digito verdadeiro
+                    return true;
+                } else {
+                    // CPF INVÁLIDO
+                    return false;
+                }
+            }
+            // caso não atende nenhuma das verificações
+            return false;
+        }
+    }
+
+    public static boolean calculaSegundoDigito(String cpf) {
+        int vetCPF[] = new int[10];
+        String posicaoNumero[] = cpf.split("");
+
+        for (int i = 0; i < vetCPF.length; i++) {
+            vetCPF[i] = Integer.parseInt(posicaoNumero[i]);
+        }
+
+        for (int i = (vetCPF.length - 1); i >= 0; i--) {
+            segundoDigito += vetCPF[i] * multiplicadorCrescente;
+            multiplicadorCrescente++;
+        }
+
+        // atualizando para 2 novamente
+        multiplicadorCrescente = 2;
+
+        if (segundoDigito % 11 < 2) {
+            // digito igual a zero
+            if (posicaoNumero[9].equals("0")) {
+                return true;
+            }else{
+                return false;
+            }
+        } else {
+            if (segundoDigito % 11 >= 2) {
+                // digito igual a 11 - resto da divisão
+                int resto = 11 - (segundoDigito % 11);
+
+                if (posicaoNumero[10].equals(Integer.toString(resto))) {
+                    //segundo digito válido
+                    return true;
+                } else {
+                    // CPF INVÁLIDO
+                    return false;
+                }
+            }
+            // caso não atende nenhuma das verificações
+            return false;
+        }
+    }
+}
